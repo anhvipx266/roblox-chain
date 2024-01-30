570d68791e211f1a28e7f5bad1400695c5e9832b
himT <61503224+anhvipx266@users.noreply.github.com>
2024-01-31 00:02:51 +0700

    xóa phần thừa

--- src/Proto.lua
+++ src/Proto.lua
@@ -12,6 +12,7 @@ Proto.__tostring = function(self) return self.ClassName end
 Proto.ClassName = 'Proto'
 Proto.ShortName = 'proto'
 --/main
+-- need overwrite
 function Proto:wrap(sp, f, ...)
     local f2 = function(...)
         print("Proto!")
@@ -19,34 +20,5 @@ function Proto:wrap(sp, f, ...)
     end
     return self:pack(self, false, f2, ...)
 end
--- thực thi một Chuỗi - Chain
--- function Proto.run(...)
---     local chain, _f = Proto.wrap(false, ...)
---     _f()
---     return chain
--- end
--- -- sinh một Chuỗi - Chain
--- function Proto.spawn(...)
---     local chain, _f = Proto.wrap(true, ...)
---     task.spawn(_f)
---     return chain
--- end
--- -- thử một Chuỗi - Chain
--- function Proto.try(f, ...)
---     local chain, _f = Proto.wrap(false, ...)
---     local success, err = pcall(_f)
---     if not success then warn("Chain err:\n", err) end
---     chain.err = err
---     return chain
--- end
---------------------------------------------------------------------------
--- short function
-Proto.r = Proto.run
-Proto.s = Proto.spawn
-Proto.t = Proto.try
-Proto.rb = Proto.runBlend
-Proto.sb = Proto.spawnBlend
-Proto.tb = Proto.tryBlend
-
 
 return setmetatable({}, Proto)
